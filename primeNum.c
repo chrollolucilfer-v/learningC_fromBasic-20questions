24143883f85edc459fcb8c3f4db05e0fc2c397fd
Bhaskar Gupta <guptabhaskar9897@gmail.com>
2026-07-14 23:57:09 +0530

    another

---
+++ primeNum.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(){
+
+    // checking whether a number is a prime number or not
+
+    int num, i, prime = 1;
+
+    printf("Enter your number");
+    scanf("%d",&num);
+
+    if( num <= 1){
+        printf("Not a Prime");
+    }
+else{
+      for(i=2; i< num; i++){
+        if(num % i ==0){
+            prime = 0;
+            break;
+
+        }
+    }
+    if(prime ==1){
+        printf("Prime");
+    }
+    else 
+    printf("not a prime")
+        
+    }
+}
+
+    return 0;
+}
